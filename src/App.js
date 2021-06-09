a920cfe9bdf94361db6344f8a3265e0204b23d31
Marco De Canal <marco.decanal@gmail.com>
2021-06-09 23:07:06 +0200

    tutorial ended

--- src/App.js
+++ src/App.js
@@ -1,37 +1,102 @@
 import React, { Component } from "react";
-import { Typography, TextField, Paper, Button } from "@material-ui/core";
-export default class App extends Component {
-  state = {
-    exercises: [],
-    title: "",
-  };
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
-  render() {
-    const { title } = this.state;
-    return (
-      <Paper>
-        <Typography variant="h4" align="center" gutterBottom>
-          Exercises
-        </Typography>
-        <form>
-          <TextField
-            name="title"
-            label="exercise"
-            value={title}
-            onChange={this.handleChange}
-            margin="normal"
-          />
-          <Button type="submit" color="primary" variant="raised">
-            Create
-          </Button>
-          <Button type="submit" color="primary" variant="raised">
-            Create
-          </Button>
-          <Button>Hello</Button>
-        </form>
-      </Paper>
-    );
+import {
+  Typography,
+  TextField,
+  Paper,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  IconButton,
+  ListItemSecondaryAction,
+} from "@material-ui/core";
+import { withStyles, createMuiTheme } from "@material-ui/core/styles";
+import { Delete } from "@material-ui/icons";
+import { orange } from "@material-ui/core/colors";
+import { findByLabelText } from "@testing-library/dom";
+
+const styles = {
+  root: {
+    padding: 20,
+    margin: 20,
+    maxWidth: 400,
+  },
+  form: {
+    display: "flex",
+    alignItems: "baseline",
+    justifyContent: "space-evenly",
+  },
+};
+
+export default withStyles(styles)(
+  class App extends Component {
+    state = {
+      exercises: [
+        {
+          title: "squat",
+          id: 1,
+        },
+        {
+          title: "biceps",
+          id: 2,
+        },
+      ],
+      title: "",
+    };
+    handleChange = ({ target: { name, value } }) => {
+      this.setState({ [name]: value });
+    };
+    handleCreate = (ev) => {
+      ev.preventDefault();
+      if (this.state.title) {
+        this.setState(({ exercises, title }) => ({
+          exercises: [...exercises, { title, id: Date.now() }],
+          title: "",
+        }));
+      }
+    };
+    handleDelete = (id) => {
+      this.setState(({ exercises }) => ({
+        exercises: exercises.filter((ex) => ex.id !== id),
+      }));
+    };
+    render() {
+      const { exercises, title } = this.state;
+      const { classes } = this.props;
+      return (
+        <Paper className={classes.root}>
+          <Typography variant="h4" align="center" gutterBottom>
+            Exercises
+          </Typography>
+          <form onSubmit={this.handleCreate} className={classes.form}>
+            <TextField
+              name="title"
+              label="exercise"
+              value={title}
+              onChange={this.handleChange}
+              margin="normal"
+            />
+            <Button type="submit" color="primary" variant="contained">
+              Create
+            </Button>
+          </form>
+          <List>
+            {exercises.map(({ title, id }) => (
+              <ListItem key={id}>
+                <ListItemText primary={title} />
+                <ListItemSecondaryAction>
+                  <IconButton
+                    color="primary"
+                    onClick={() => this.handleDelete(id)}
+                  >
+                    <Delete />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      );
+    }
   }
-}
+);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { orange } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: orange,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
